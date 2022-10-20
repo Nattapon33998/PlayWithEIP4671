df78b9c7e217657082a475258a0a1e93fa7bec13
Nattapon Sukthaworn <Oat@admins-MacBook-Pro.local>
2022-10-21 02:22:17 +0700

    init

---
+++ contracts/CreatorBadge.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./ERC4671.sol";
+
+contract CreatorBadge is ERC4671 {
+    constructor() ERC4671("EIP Creator Badge", "EIP") {}
+
+    function giveThatManABadge(address owner) external {
+        require(_isCreator(), "You must be the contract creator");
+        _mint(owner);
+    }
+}
---
+++ deploy/CreatorBadge.js
@@ -0,0 +1,13 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const BadgeContract = await ethers.getContractFactory("CreatorBadge");
+  const badgeContract = await BadgeContract.deploy("Oat");
+
+  console.log("My Badge Contract addess is : ", badgeContract.address);
+}
+
+main().catch((error) => {
+  console.log(error);
+  process.exitCode = 1;
+});
